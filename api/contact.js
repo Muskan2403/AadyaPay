326e0dc2921b15d0d6442478c40d9a033f714c6a
Muskan2403 <muskandarji24@gmail.com>
2025-10-03 01:41:20 +0530

    updated contact

--- api/contact.js
+++ api/contact.js
@@ -13,12 +13,14 @@ export default async function handler(req, res) {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.sendMail({
       from: email,
